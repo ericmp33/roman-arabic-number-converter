dd1e62efea2d0fe04613c7a02252a60f09959443
ericmp <ericmorenopou@gmail.com>
2022-06-20 21:05:40 +0200

    refactored Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,11 @@ public class Main {
     public static void main(String[] args) {
         var line = "-------------------------------------------------------------";
 
-        // print welcome text
         System.out.println(line);
         System.out.println("Roman to Arabic / Arabic to Roman number converter");
         System.out.println(line);
         System.out.println("Input numbers between 1 and 1000 (Roman or Arabic format)");
 
-        // ask user input
         var sc = new Scanner(System.in);
 
         while (true) {
@@ -24,8 +22,9 @@ public class Main {
                 return;
             }
 
-            // check if user wants to do roman to arabic or inverse
-            if (getConversionMode(input).equals("arabicToRoman")) {
+            // if user inputted a valid positive int
+            if (isValidPositiveInt(input)) {
+                // convert from Arabic to Roman
                 var validatedInput = ArabicToRoman.validate(input);
 
                 if (! validatedInput.equals("failed")) {
@@ -34,6 +33,7 @@ public class Main {
                     System.out.println("Invalid input");
                 }
             } else {
+                // convert from Roman to Arabic
                 var validatedInput = RomanToArabic.validate(input);
 
                 if (! validatedInput.equals("failed")) {
@@ -45,15 +45,6 @@ public class Main {
 
             System.out.println();
         }
-    }
-
-    private static String getConversionMode(String input) {
-        // if input is a valid and positive Integer
-        if (isValidPositiveInt(input)) {
-            return "arabicToRoman";
-        }
-
-        return "romanToArabic";
     }
 
     // returns true if parsed var is an integer and positive
